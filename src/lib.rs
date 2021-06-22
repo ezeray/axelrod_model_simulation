7149e122d06bde01870533707aeec398909dc3fe
Ezequiel Raigorodsky <ezequielraigorodsky@gmail.com>
2021-06-22 20:23:43 -0300

    making changes to make the code more idiomatic

--- src/lib.rs
+++ src/lib.rs
@@ -12,17 +12,20 @@ pub struct SimulationConfig {
 	pub num_traits: u32,
 	pub width: u32,
 	pub height: u32,
+	pub num_iterations: usize,
 	pub rng: rand_pcg::Lcg128Xsl64,
 }
 
 impl SimulationConfig {
-	pub fn new(num_features: u32, num_traits: u32, width: u32, height: u32, rng: rand_pcg::Lcg128Xsl64) -> SimulationConfig {
+	pub fn new(num_features: u32, num_traits: u32, width: u32, height: u32, num_iterations: usize, rng: rand_pcg::Lcg128Xsl64) -> SimulationConfig {
 		SimulationConfig {
 			num_features,
 			num_traits,
 			width,
 			height,
+			num_iterations,
 			rng,
+
 		}
 	}
 }
@@ -51,7 +54,9 @@ impl Point {
 pub struct Territory {
 	width: u32,
 	height: u32,
-	pub territory: Vec<Vec<Individual>>
+	pub territory: Vec<Vec<Individual>>,
+	// pub culture_counter: Option<HashMap<Vec<u32>, u32>>,
+	// pub culture_labels: Option<HashMap<Vec<u32>, u32>>,
 }
 
 impl Territory {
@@ -73,6 +78,68 @@ impl Territory {
 		Territory { width: config.width, height: config.height, territory }
 	}
 
+	pub fn run_simulation(config: &mut SimulationConfig) -> Territory {
+		let mut simulation = Territory::new(config);
+
+		for _ in 0..config.num_iterations {
+			// too much fighting with borrow checker, will look ugly but it's the best I can do at this point
+			// get coordinates for individual
+			let (x, y): (usize, usize) = (config.rng.gen_range(0..config.width) as usize, config.rng.gen_range(0..config.height) as usize);
+	
+			let chosen = &simulation.territory[x][y];
+			let loc_neighbor = chosen.choose_random_neighbor(config);
+			let neighbor = simulation.return_neighbor_clone(loc_neighbor);
+	
+			let chosen = &mut simulation.territory[x][y];
+			chosen.interact(neighbor, config);
+		}
+	
+		// let file = File::create("./simulation_terrain.json").unwrap();
+		// serde_json::to_writer(&file, &simulation).unwrap();
+	
+		simulation
+	}
+
+	pub fn run_count_cultures(&self, config: &SimulationConfig) -> HashMap<Vec<u32>, u32> {
+		let mut culture_hashmap: HashMap<Vec<u32>, u32> = HashMap::new();
+	
+		for i in 0..config.width {
+			for j in 0..config.height {
+				let count = culture_hashmap
+					.entry(self.territory[i as usize][j as usize].clone_cultural_features())
+					.or_insert(0);
+				*count += 1;
+	
+			}
+		}
+	
+		culture_hashmap
+	}
+
+	pub fn run_assign_label_to_cultures(&self, config: &SimulationConfig) -> HashMap<Vec<u32>, u32> {
+		let mut current_label: u32 = 1;
+		let mut map: HashMap<Vec<u32>, u32> = HashMap::new();
+		
+		for i in 0..config.width {
+			for j in 0..config.height {
+				let current_features = self.territory[i as usize][j as usize].clone_cultural_features();
+				if !map.contains_key(&current_features) {
+					map.insert(current_features, current_label);
+				}
+				current_label += 1;
+				/* 
+				 * 
+				 */
+			}
+		}
+
+		map
+	}
+	
+	pub fn calc_prop_pop_in_largest_cluster(config: &SimulationConfig, map: &HashMap<Vec<u32>, u32>) -> f32 {
+		*map.values().max().unwrap() as f32 / config.height.pow(2) as f32
+	}
+
 	pub fn return_neighbor_clone(&self, location: &Point) -> Individual {
 		let neighbor = &self.territory[location.x()][location.y()];
 
@@ -84,11 +151,32 @@ impl Territory {
 	}
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FinishedTerritory {
+	width: u32,
+	height: u32,
+	pub territory: Vec<Vec<Individual>>,
+	pub culture_counter: HashMap<Vec<u32>, u32>,
+	pub culture_labels: HashMap<Vec<u32>, u32>,
+}
+
+impl FinishedTerritory {
+	pub fn new(config: &SimulationConfig, simulation: Territory) -> FinishedTerritory {
+		let culture_counter = simulation.run_count_cultures(config);
+		let culture_labels = HashMap::new();
+		let width = simulation.width;
+		let height = simulation.height;
+		let territory = simulation.territory;
+		FinishedTerritory { width, height, territory, culture_counter, culture_labels }
+	}
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Individual {
 	location: Point,
 	cultural_features: Vec<u32>,
 	neighbors: Vec<Point>,
+	// cluster: Option<u32>,
 }
 
 impl Individual {
@@ -184,49 +272,6 @@ impl Individual {
 	pub fn clone_cultural_features(&self) -> Vec<u32> {
 		self.cultural_features.clone()
 	}
-}
-
-pub fn run_simulation(config: &mut SimulationConfig) -> Territory {
-	let mut simulation = Territory::new(config);
-
-	for _ in 0..1_000_000 {
-		// too much fighting with borrow checker, will look ugly but it's the best I can do at this point
-        // get coordinates for individual
-        let (x, y): (usize, usize) = (config.rng.gen_range(0..config.width) as usize, config.rng.gen_range(0..config.height) as usize);
-
-        let chosen = &simulation.territory[x][y];
-        let loc_neighbor = chosen.choose_random_neighbor(config);
-        let neighbor = simulation.return_neighbor_clone(loc_neighbor);
-
-        let chosen = &mut simulation.territory[x][y];
-        chosen.interact(neighbor, config);
-	}
-
-	// let file = File::create("./simulation_terrain.json").unwrap();
-    // serde_json::to_writer(&file, &simulation).unwrap();
-
-	simulation
-}
-
-pub fn run_count_cultures(config: &SimulationConfig, simulation: &Territory) -> HashMap<Vec<u32>, u32> {
-	let mut culture_hashmap: HashMap<Vec<u32>, u32> = HashMap::new();
-
-    for i in 0..config.width {
-        for j in 0..config.height {
-            // simulation.territory[i as usize][j as usize].print_cultural_features();
-            let count = culture_hashmap
-                .entry(simulation.territory[i as usize][j as usize].clone_cultural_features())
-                .or_insert(0);
-            *count += 1;
-
-        }
-    }
-
-	// for (k, v) in culture_hashmap.iter() {
-    //     println!("Cultural features combination {:?} appeared {} times", k, v);
-    // }
-
-	culture_hashmap
 }
 
 #[cfg(Tests)]
